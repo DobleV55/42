1a4e5db719f7f5d4ca60410ff6ca464c1f194157
Valentin Vila <vilavalentin@gmail.com>
2022-10-14 03:08:12 +0200

    %c and %s finished

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -5,25 +5,24 @@ void	norm_putchar(char c)
 	write (1, &c, 1);
 }
 
-char const	*ft_putuint(char const *str, unsigned int num)
+int	ft_putuint(unsigned int num)
 {
 	if (!num)
-		return (++str);
+		return (1);
 	if (num >= 10)
 	{
-		ft_putuint(str, num / 10);
-		ft_putuint(str, num % 10);
+		ft_putuint(num / 10);
+		ft_putuint(num % 10);
 	}
 	else
 		norm_putchar(num + '0');
-	str++;
-	return (str);
+	return (1);
 }
 
-char const	*ft_putnbr(char const *str, int num)
+int	ft_putnbr(int num)
 {
 	if (!num)
-		return (++str);
+		return (1);
 	if (num < 0)
 	{
 		norm_putchar('-');
@@ -31,34 +30,40 @@ char const	*ft_putnbr(char const *str, int num)
 	}
 	if (num >= 10)
 	{
-		ft_putnbr(str, num / 10);
-		ft_putnbr(str, num % 10);
+		ft_putnbr(num / 10);
+		ft_putnbr(num % 10);
 	}
 	else
 		norm_putchar(num + '0');
-	str++;
-	return (str);
+	return (1);
 }
 
-char const	*ft_putstr(char const *str, char *s)
+int	ft_putstr(char *s)
 {
-	while (*s)
+	int	len;
+
+	len = 0;
+	if (!s)
 	{
-		write(1, s, 1);
-		s++;
+		write(1, "(null)", 6);
+		return (6);
 	}
-	str++;
-	return (str);
+	while (s[len])
+	{
+		write(1, &s[len], 1);
+		len++;
+	}
+	return (len);
 }
 
-char const	*ft_putchar(char const *str, char c)
+int	ft_putchar(char c)
 {
 	write(1, &c, 1);
-	str++;
-	return (str);
+	return (1);
 }
 
-char const	*ft_puthexnbr(char const *str, int num, int is_lower) {
+int	ft_puthexnbr(int num, char format)
+{
 
 	char	*hexchar_lower;
 	char	*hexchar_upper;
@@ -67,62 +72,83 @@ char const	*ft_puthexnbr(char const *str, int num, int is_lower) {
 	hexchar_upper = "0123456789ABCDEF";
 	if (num >= 16)
 	{
-		ft_puthexnbr(str, num / 16, is_lower);
-		ft_puthexnbr(str, num % 16, is_lower);
+		ft_puthexnbr(num / 16, format);
+		ft_puthexnbr(num % 16, format);
 	}
-	else if (is_lower)
+	else if (format == 'x')
 		norm_putchar(hexchar_lower[num % 16]);
 	else
 		norm_putchar(hexchar_upper[num % 16]);
-	str++;
-	return (str);
+	return (1);
 }
 
-char const	*ft_putaddress(char const *str, unsigned long num) {
+int	ft_putaddress(unsigned long num)
+{
 
 	char	*hexchar;
 
 	hexchar = "0123456789abcdef";
 	if (num >= 16)
 	{
-		ft_putaddress(str, num / 16);
-		ft_putaddress(str, num % 16);
+		ft_putaddress(num / 16);
+		ft_putaddress(num % 16);
 	}
 	else
 		norm_putchar(hexchar[num % 16]);
-	str++;
-	return (str);
+	return (1);
+}
+
+int	ft_format(va_list ap, const char str)
+{
+	int	prints;
+
+	prints = 0;
+	if (str == 'i' || str == 'd')
+		prints += ft_putnbr(va_arg(ap, int));
+	else if (str == 'p')
+	{
+		norm_putchar('0');
+		norm_putchar('x');
+		prints += 2;
+		prints += ft_putaddress(va_arg(ap, unsigned long long));
+	}
+	else if (str == 'u')
+		prints += ft_putnbr(va_arg(ap, unsigned int));
+	else if (str == 'x' || str == 'X')
+		prints += ft_puthexnbr(va_arg(ap, int), str);
+	return (prints);
 }
 
 int ft_printf(const char *str, ...)
 {
+	int	i;
+	int	prints;
 	va_list ap;
 
+	i = 0;
+	prints = 0;
 	va_start(ap, str);
-	while (*str)
+	while (str[i])
 	{
-		if (*str == '%' && *(str+1) == 's')
-			str = ft_putstr(str, va_arg(ap, char*));
-		else if (*str == '%' && (*(str+1) == 'i' || *(str+1) == 'd'))
-			str = ft_putnbr(str, va_arg(ap, int));
-		else if (*str == '%' && *(str+1) == 'c')
-			str = ft_putchar(str, va_arg(ap, int));
-		else if (*str == '%' && *(str+1) == 'p')
+		if (str[i] == '%' && str[i+1] == 'c')
 		{
-			norm_putchar('0');
-			norm_putchar('x');
-			str = ft_putaddress(str, va_arg(ap, unsigned long));
+			prints += ft_putchar(va_arg(ap, int));
+			i++;
 		}
-		else if (*str == '%' && *(str+1) == 'u')
-			str = ft_putnbr(str, va_arg(ap, unsigned int));
-		else if (*str == '%' && *(str+1) == 'x')
-			str = ft_puthexnbr(str, va_arg(ap, int), 1);
-		else if (*str == '%' && *(str+1) == 'X')
-			str = ft_puthexnbr(str, va_arg(ap, int), 0);
+		else if (str[i] == '%' && str[i+1] == 's')
+		{
+			prints += ft_putstr(va_arg(ap, char*));
+			i++;
+		}
+		else if (str[i] == '%')
+		{
+			prints += ft_format(ap, str[i+1]);
+			i++;
+		}
 		else
-			write(1, str, 1);
-		str++;
+			prints += ft_putchar(str[i]);
+		i++;
 	}
 	va_end(ap);
-	return (0);
+	return (prints);
 }
