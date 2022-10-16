4b2fb15e95075354464032bdf5fb4d7225b49722
Valentin Vila <vvila@c3r13s2.42madrid.com>
2022-10-16 22:08:41 +0200

    finished

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,4 +1,127 @@
 #include "ft_printf.h"
+#include <string.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str_cpy;
+	int		len;
+
+	len = 0;
+	while (*s1)
+	{
+		s1++;
+		len++;
+	}
+	str_cpy = (char *) malloc (len + 1);
+	if (!str_cpy)
+		return (0);
+	s1 = s1 - len;
+	while (*s1)
+	{
+		*str_cpy = *s1;
+		str_cpy++;
+		s1++;
+	}
+	*str_cpy = '\0';
+	return (str_cpy - len);
+}
+
+static	int	int_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+static	unsigned int	uint_len(unsigned int n)
+{
+	unsigned int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int		len_n;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len_n = int_len(n);
+	res = (char *) malloc (len_n + 1);
+	if (!res)
+		return (NULL);
+	if (n < 0)
+	{
+		n = n * -1;
+		res[0] = '-';
+	}
+	res[len_n--] = '\0';
+	if (n == 0)
+		res[0] = '0';
+	while (n > 0)
+	{
+		res[len_n--] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (res);
+}
+char	*ft_uitoa(unsigned int n)
+{
+	char	*res;
+	unsigned int		len_n;
+
+	if (n == 2147483648)
+		return (ft_strdup("2147483648"));
+	len_n = uint_len(n);
+	res = (char *) malloc (len_n + 1);
+	if (!res)
+		return (NULL);
+	if (n < 0)
+	{
+		n = n * -1;
+		res[0] = '-';
+	}
+	res[len_n--] = '\0';
+	if (n == 0)
+		res[0] = '0';
+	while (n > 0)
+	{
+		res[len_n--] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (res);
+}
+
 
 void	norm_putchar(char c)
 {
@@ -24,79 +147,123 @@ int	ft_putuint(unsigned int num)
 		norm_putchar(num + '0');
 	return (1);
 }
-
-int	ft_putnbr(int num)
+int	ft_putstr(char *s, int order)
 {
-	static int	num_len;
+	int	i;
 
-	if (num == -2147483648)
-		return write(1, "-2147483648", 11);
-	if (num < 0)
-	{
-		num_len = 0;
-		num_len += ft_putchar('-');
-		num = num * -1;
-	}
-	if (num >= 10)
-	{
-		ft_putnbr(num / 10);
-		num = num % 10;
-	}
-	num_len += ft_putchar(num + '0');
-	return (num_len);
-}
-
-int	ft_putunbr(unsigned int num)
-{
-	static int	num_len;
-
-	if (num == 2147483648)
-		return write(1, "2147483648", 10);
-	if (num >= 10)
-	{
-		num_len += ft_putnbr(num / 10);
-		num = num % 10;
-	}
-	num_len += ft_putchar(num + '0');
-	return (num_len);
-}
-
-int	ft_putstr(char *s)
-{
-	int	len;
-
-	len = 0;
+	if (order == -1)
+		i = ft_strlen(s) - 1;
+	else
+		i = 0;
 	if (!s)
 	{
 		write(1, "(null)", 6);
 		return (6);
 	}
-	while (s[len])
+	while (s[i])
 	{
-		write(1, &s[len], 1);
-		len++;
+		write(1, &s[i], 1);
+		i+=(1*order);
 	}
-	return (len);
+	return (ft_strlen(s));
 }
 
-int	ft_puthexnbr(unsigned int num, char format)
+int	ft_putnbr(int num)
 {
-	static int	num_len;
-	char		*hexchar_lower;
-	char		*hexchar_upper;
+	char	*n;
+	int		len_num;
 
-	hexchar_lower = "0123456789abcdef";
-	hexchar_upper = "0123456789ABCDEF";
-	if (num >= 16)
+	n = ft_itoa(num);
+	ft_putstr(n, 1);
+	len_num = strlen(n);
+	if (n)
+		free(n);
+	return (len_num);
+}
+
+int	ft_putunbr(unsigned int num)
+{
+	char	*n;
+	int		len_num;
+
+	n = ft_uitoa(num);
+	ft_putstr(n, 1);
+	len_num = ft_strlen(n);
+	if (n)
+		free(n);
+	return (len_num);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+
+	str = s;
+	while (n > 0)
 	{
-		ft_puthexnbr(num / 16, format);
-		num = num % 16;
+		*str = 0;
+		str++;
+		n--;
 	}
-	if (format == 'x')
-		num_len += ft_putchar(hexchar_lower[num % 16]);
-	else
-		num_len += ft_putchar(hexchar_upper[num % 16]);
-	return (num_len);
+}
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*r;
+
+	if (count == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
+	r = (void *) malloc (count * size);
+	if (!r)
+		return (NULL);
+	ft_bzero(r, count * size);
+	return (r);
+}
+int	ft_puthexnbr_upper(unsigned int num)
+{
+	int			temp;
+	int			i;
+	char 		*hex_num;
+
+	if (!num)
+		return (ft_putstr("0", 1));
+	hex_num = ft_calloc(1,9);
+	i = 0;
+	while (num > 0)
+	{
+		temp = num % 16;
+		if (temp < 10)
+			hex_num[i++] = temp + 48;
+		else
+			hex_num[i++] = temp + 55;
+		num = num / 16;
+	}
+	ft_putstr(hex_num, -1);
+	free(hex_num);
+	return (i);
+}
+
+int	ft_puthexnbr_lower(unsigned int num)
+{
+	int			temp;
+	int			i;
+	char 		*hex_num;
+
+	if (!num)
+		return (ft_putstr("0", 1));
+	hex_num = ft_calloc(1,9);
+	i = 0;
+	while (num > 0)
+	{
+		temp = num % 16;
+		if (temp < 10)
+			hex_num[i++] = temp + 48;
+		else
+			hex_num[i++] = temp + 87;
+		num = num / 16;
+	}
+	ft_putstr(hex_num, -1);
+	free(hex_num);
+	return (i);
 }
 
 int	ft_putaddress(unsigned long num)
@@ -126,10 +293,13 @@ static int	ft_format(va_list *ap, const char str)
 	}
 	else if (str == 's')
 	{
-		prints += ft_putstr(va_arg(*ap, char*));
+		prints += ft_putstr(va_arg(*ap, char*), 1);
 	}
 	else if (str == 'i' || str == 'd')
+	{
 		prints += ft_putnbr(va_arg(*ap, int));
+	}
+
 	else if (str == 'p')
 	{
 		write(1, "0x", 2);
@@ -138,8 +308,10 @@ static int	ft_format(va_list *ap, const char str)
 	}
 	else if (str == 'u')
 		prints += ft_putunbr(va_arg(*ap, unsigned int));
-	else if (str == 'x' || str == 'X')
-		prints += ft_puthexnbr(va_arg(*ap, int), str);
+	else if (str == 'x')
+		prints += ft_puthexnbr_lower(va_arg(*ap, int));
+	else if (str == 'X')
+		prints += ft_puthexnbr_upper(va_arg(*ap, int));
 	else if (str == '%')
 		prints += ft_putchar('%');
 	return (prints);
