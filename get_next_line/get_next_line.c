34ed2e24ee8e28650351bacfb63d7e026327530b
Valentin Vila <vilavalentin@gmail.com>
2022-11-29 18:10:06 +0100

    partially remove tmp variable

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,51 +1,49 @@
 #include "get_next_line.h"
 
-void	map_response_and_backup(char *tmp, char **response, char **backup)
+void	map_response_and_backup(char **response, char **backup)
 {
 	char	*res;
 	char	*back;
 	int		i;
 	int		j;
 
-	tmp = ft_strdup(*backup);
-	ft_free_all(NULL, backup, NULL);
 	i = 0;
-	while (tmp[i] != '\0' && tmp[i] != '\n')
+	while ((*backup)[i] != '\0' && (*backup)[i] != '\n')
 		i++;
-	if (tmp[i] == '\n')
+	if ((*backup)[i] == '\n')
 		i++;
 	res = ft_calloc(i + 1, 1);
 	if (!res)
 		return ;
 	i = 0;
-	while (tmp[i] != '\0' && tmp[i] != '\n')
+	while ((*backup)[i] != '\0' && (*backup)[i] != '\n')
 	{
-		res[i] = tmp[i];
+		res[i] = (*backup)[i];
 		i++;
 	}
-	if (tmp[i] == '\n')
+	if ((*backup)[i] == '\n')
 		res[i] = '\n';
 	*response = res;
 
 	j = 0;
-	while (tmp && tmp[j])
+	while (*backup && (*backup)[j])
 		j++;
 	i = 0;
-	while (tmp[i] != '\0' && tmp[i] != '\n')
+	while ((*backup)[i] != '\0' && (*backup)[i] != '\n')
 		i++;
-	if (tmp[i] == '\n')
+	if ((*backup)[i] == '\n')
 		i++;
 	back = ft_calloc((j - i) + 1, 1);
 	if (!back)
 		return ;
 	j = 0;
-	while (tmp[i + j])
+	while ((*backup)[i + j])
 	{
-		back[j] = tmp[i + j];
+		back[j] = (*backup)[i + j];
 		j++;
 	}
+	ft_free_all(NULL, backup, NULL);
 	*backup = back;
-	ft_free_all(&tmp, NULL, NULL);
 	return ;
 }
 void	ft_free_all(char **buffer, char **backup, char **response)
@@ -67,10 +65,11 @@ void	ft_free_all(char **buffer, char **backup, char **response)
 	}
 }
 
-void	ft_read_file(int fd, char **backup, char **tmp)
+void	ft_read_file(int fd, char **backup)
 {
 	int 		chars_read;
 	char		*buffer;
+	char		*tmp;
 
 	chars_read = 1;
 	buffer = (char *) malloc (BUFFER_SIZE + 1);
@@ -80,19 +79,16 @@ void	ft_read_file(int fd, char **backup, char **tmp)
 	{
 		chars_read = read(fd, buffer, BUFFER_SIZE);
 		if (chars_read < 0)
-		{
-			ft_free_all(&buffer, backup, tmp);
-			return ;
-		}
+			return ft_free_all(&buffer, backup, NULL);
 		buffer[chars_read] = '\0';
 		// copio backup en tmp
-		*tmp = ft_strdup(*backup);
+		tmp = ft_strdup(*backup);
 		// libero backup
 		ft_free_all(NULL, backup, NULL);
 		// backup -> tmp + buffer
-		*backup = ft_strjoin(*tmp, buffer);
+		*backup = ft_strjoin(tmp, buffer);
 		// libero tmp
-		ft_free_all(tmp, NULL, NULL);
+		ft_free_all(&tmp, NULL, NULL);
 		// se podria hacer: buffer = buffer + backup -> libero backup -> backup = buffer -> libero buffer (o no :c)
 		// buffer = ft_strjoin(*backup, buffer);
 		// ft_free_all(NULL, backup, NULL);
@@ -108,18 +104,16 @@ char	*get_next_line(int fd)
 {
 	char static	*backup = NULL;
 	char		*response;
-	char		*tmp;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	response = NULL;
-	tmp = NULL;
-	ft_read_file(fd, &backup, &tmp);
+	ft_read_file(fd, &backup);
 	if (backup != NULL && *backup != '\0')
-		map_response_and_backup(tmp, &response, &backup);
+		map_response_and_backup(&response, &backup);
 	if (!response || *response == '\0')
 	{
-		ft_free_all(&tmp, &backup, &response);
+		ft_free_all(NULL, &backup, &response);
 		return (NULL);
 	}
 	return (response);
